4a6547aaa77768d8a478e5ee394915cd1c5d6b77
b99203022 <b99203022@ntu.edu.tw>
2015-06-11 16:22:10 +0800

    add building.java

---
+++ src/Building.java
@@ -0,0 +1,26 @@
+
+public class Building {
+	private int level;
+	private char color;
+	private int point;
+	
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public char getColor() {
+		return color;
+	}
+	public void setColor(char color) {
+		this.color = color;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+}
