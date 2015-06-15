5662d0d9758384648b18c4acb58a8a74ee973af2
Max <max80713@hotmail.com>
2015-06-15 16:36:36 +0800

    modified

--- src/Building.java
+++ src/Building.java
@@ -1,34 +1,57 @@
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
 
-public class Building {
+public class Building extends JPanel{
 	private int point;
 	private int level;
 	private Color color;
 	private ItemOnField item;
 	
-	public Building(int point,Color color){
-		this.point=point;
-		this.item=ItemOnField.people;
-		this.color=color;
+	private int col;
+	private int row;
+	public boolean isSearched;
+	public boolean toBeMerged;
+
+	private int x;
+	private int y;
+
+	public static final int WIDTH = 100;
+	public static final int HEIGHT = 100;
+	public static final int LEFT_BOARDER = 50;
+	public static final int UPPER_BOARDER = 200;
+	
+	public Building(int col,int row){
+		super();
+		point=randomPoint();
+		// level=
+		// item=ItemOnField.people;	
+		color=randomColor();
+		isSearched = false;
+		toBeMerged = false;
+		
+		this.col = col;
+		this.row = row;
+		x = LEFT_BOARDER + col*WIDTH;
+    	y = UPPER_BOARDER + row*HEIGHT;
+    	setBackground(color);
+    	setLocation(x, y);
+        setSize(WIDTH, HEIGHT);
 	}	
-	public Building(){
-		this(1,Color.green);		
+	public int getCol(){
+		return col;
 	}
-
-
-	public int getLevel() {
-		return level;
+	public int getRow(){
+		return row;
 	}
-
 	public Color getColor() {
 		return color;
-	}
-	public void setColor(Color color) {
-		this.color = color;
 	}
 	public int getPoint() {
 		return point;
 	}
-	public void setPoint(int point) {
+	public void setPoint(int point) {	//to be modified
 		this.point = point;
 		if (this.point==1){
 			this.item=ItemOnField.people;
@@ -38,14 +61,46 @@ public class Building {
 			this.item=ItemOnField.house;
 		}else {
 			this.item=ItemOnField.building;
-
 		}
 	}
-	public void combinebuilding(Building[] buildings){
-		int p=this.point;
-		for (Building b:buildings){
-			p+=b.getPoint();
+	public int getLevel() {
+		return level;
+	}
+	public void resetRow(int row){
+		this.row = row;
+	}
+	public void resetLocation(){
+		x = 50 + col*100;
+    	y = 200 + row*100;
+    	setLocation(x, y);
+	}
+	public void addPoint(Building merged){
+		point += merged.getPoint();
+		level = (int) ( Math.log(point) / Math.log(2) ) ;	//to be modified
+	}
+	//generate random color
+	private static Color randomColor(){
+		int r = (int) (Math.random() * 3);
+		switch (r){
+			case 0:
+				return Color.green;
+			case 1:
+				return Color.gray;
+			case 2:
+				return new Color(200, 150, 0);
+			default:
+				return null;
 		}
-		this.setPoint(p);
-	}	
+	}
+	//generate random point
+	private static int randomPoint(){
+		int r = (int) (Math.random() * 2) + 1;
+		return r;
+	}
+	//show point
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setFont(new Font("Monospaced", Font.BOLD, 72));
+        g.drawString(Integer.toString(point), 10, 80);
+    } 
 }
--- src/City.java
+++ src/City.java
@@ -1,12 +1,198 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class City {
-	
-	public static void main(String args[]){
-			Building[] buildingList=new Building[25];
-			for (int i=0;i<buildingList.length;i++){
-				buildingList[i]=new Building();
+public class City extends JFrame{
+	public static final int FRAME_WIDTH = 600;
+	public static final int FRAME_HEIGHT = 800;
+	public static final int PANEL_WIDTH = 500;
+	public static final int PANEL_HEIGHT = 100;
+	public static final int PANEL_X = 50;
+	public static final int PANEL_Y = 50;
+
+	public JPanel panel;
+	public JLabel pointLabel;
+	public JLabel moveLabel;
+	public Building[][] buildings = new Building[5][5];
+	private int point;
+	private int move;
+
+	public City(){
+		super();
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		setTitle("SubaraCity");
+		setLayout(null);
+		setVisible(true);
+		
+		for(int col = 0; col < 5; col++){
+			for(int row = 0 ; row < 5 ; row++){
+				buildings[col][row] = new Building(col, row);
+				buildings[col][row].addMouseListener(new ActionMerge(buildings[col][row]));
+				add(buildings[col][row]);
 			}
+		}
+
+		panel = new JPanel();
+		panel.setLocation(PANEL_X, PANEL_Y);
+        panel.setSize(PANEL_WIDTH, PANEL_HEIGHT);
+        panel.setLayout(new GridLayout(1, 2));
+
+        add(panel);
+
+        updatePoint();
+        pointLabel = new JLabel();
+        pointLabel.setText("Point : " + point);
+        pointLabel.setFont(new Font("Monospaced", Font.BOLD, 24));
+        panel.add(pointLabel);
+        
+        move = 0;
+        moveLabel = new JLabel();
+        moveLabel.setText("Moves : " + move);
+        moveLabel.setFont(new Font("Monospaced", Font.BOLD, 24));
+        panel.add(moveLabel);
+
+	}
+	
+	public void click(Building b){
+		search(b);
+		merge(b);
+		fall();
+		fill();
+		updatePoint();
+		move++;
+	}
+	//search for same-color building to be merged
+	public void search(Building b){
+		searchNear(b);
+
+		for(int i = 0; i < 5; i++){
+			for(int j = 0 ; j < 5 ; j++){
+				buildings[i][j].isSearched = false;	//reset
+			}
+		}
+	}
+	//search for nearby same-color building
+	public void searchNear(Building b){
+		int row = b.getRow();
+		int col = b.getCol();
+		Color color = b.getColor();
+		buildings[col][row].isSearched = true;	//avoid self search
+		//search up
+		if( row != 0 && buildings[col][row-1].isSearched == false ){
+			buildings[col][row-1].isSearched = true; 
+			if( color.equals( buildings[col][row-1].getColor() ) ){
+				buildings[col][row-1].toBeMerged = true;
+				searchNear(buildings[col][row-1]);
+			}
+		}
+		//search down
+		if( row != 4 && buildings[col][row+1].isSearched == false ){
+			buildings[col][row+1].isSearched = true;  
+			if( color.equals( buildings[col][row+1].getColor() ) ){
+				buildings[col][row+1].toBeMerged = true;
+				searchNear(buildings[col][row+1]);
+			}
+		}
+		//search left
+		if( col != 0 && buildings[col-1][row].isSearched == false ){
+			buildings[col-1][row].isSearched = true; 
+			if( color.equals( buildings[col-1][row].getColor() ) ){
+				buildings[col-1][row].toBeMerged = true;
+				searchNear(buildings[col-1][row]);
+			}
+		}
+		//search right
+		if( col != 4 && buildings[col+1][row].isSearched == false ){
+			buildings[col+1][row].isSearched = true; 
+			if( color.equals( buildings[col+1][row].getColor() ) ){
+				buildings[col+1][row].toBeMerged = true;
+				searchNear(buildings[col+1][row]);
+			}
+		}
+	}	
+	//merge same-color building
+	public void merge(Building b){
+		for(int i = 0; i < 5; i++){
+			for(int j = 0 ; j < 5 ; j++){
+				if(buildings[i][j].toBeMerged == true){
+					b.addPoint(buildings[i][j]);
+					remove(buildings[i][j]);
+					buildings[i][j] = null;
+				}
+			}
+		}
+	}
+	//fall the building
+	public void fall(){
+		for(int col = 0; col < 5; col++){
+			colFall(buildings[col]);
+		}
+	}
+	public void colFall(Building[] colBuildings){
+		Building[] tmp = new Building[5];
+		int rowTmp = 4;
+		for(int row = 4; row >= 0; row--){
+			if(colBuildings[row] != null){
+				tmp[rowTmp] = colBuildings[row];
+				tmp[rowTmp].resetRow(rowTmp);
+				tmp[rowTmp].resetLocation();
+				rowTmp--;
+			}
+		}
+		for(int row = 0; row < 5; row++){
+			colBuildings[row] = tmp[row];
+		}
+	}
+	//fill the blank
+	public void fill(){
+		for(int col = 0; col < 5; col++){
+			colFill(buildings[col], col);
+		}
+	}
+	public void colFill(Building[] colBuildings, int col){
+		for(int row = 0; row < 5; row++){
+			if(colBuildings[row] == null){
+				colBuildings[row] = new Building(col, row); 
+				buildings[col][row].addMouseListener(new ActionMerge(buildings[col][row]));
+				add(buildings[col][row]);			
+			}
+		}
+	}
+	
+	public void updatePoint(){
+		int tmp = 0;
+		for(int i = 0; i < 5; i++){
+			for(int j = 0 ; j < 5 ; j++){
+				tmp += buildings[i][j].getPoint();	
+			}
+		}
+		point = tmp;
 	}
 
 
+	private class ActionMerge extends MouseAdapter{
+		private Building b;
+		public ActionMerge(Building b){
+			super();
+			this.b = b;
+		}
+		public void mouseClicked(MouseEvent e){
+			click(b);
+			repaint();
+			pointLabel.setText("Point : " + point);
+        	moveLabel.setText("Moves : " + move);
+			panel.repaint();
+			// try{
+			// 	Thread.sleep(2000);
+			// }
+			// catch(InterruptedException ex){
+
+			// }
+		}
+	}
 }
